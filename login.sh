652b61aec52babc5399f2de24365a6b56b628719
Escorpio28-Oficial <80615910+Scorpio28-Official@users.noreply.github.com>
2021-07-28 22:37:47 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -1275,6 +1275,59 @@ apt install python
 apt install python2
 pip2 install colorama
 chmod +x main_linux.py
+cd $HOME
+echo -e "
+\e[1;30m┌════════════════┐
+█\e[1;32m PhoneInfoga... \e[1;30m█
+└════════════════┘\e[0m"
+echo -e ""
+pkg install -y python
+pip install --upgrade pip
+git clone https://github.com/Scorpio28-Official/PhoneInfoga
+cd PhoneInfoga
+mv config.example.py config.py
+python -m pip install -r requirements.txt
+cd $HOME
+echo -e "
+\e[1;30m┌═════════════════════┐
+█\e[1;32m Optiva-Framework... \e[1;30m█
+└═════════════════════┘\e[0m"
+echo -e ""
+pkg install -y python2
+pip2 install --upgrade pip
+git clone https://github.com/Scorpio28-Official/Optiva-Framework
+cd Optiva-Framework
+chmod 777 installer.sh
+cd $HOME
+echo -e "
+\e[1;30m┌════════════┐
+█\e[1;32m tmvenom... \e[1;30m█
+└════════════┘\e[0m"
+git clone https://github.com/Scorpio28-Official/tmvenom
+cd tmvenom
+chmod +x *
+sh install.sh
+cd $HOME
+echo -e "
+\e[1;30m┌════════════┐
+█\e[1;32m Geonumb... \e[1;30m█
+└════════════┘\e[0m"
+git clone https://github.com/Scorpio28-Official/Geonumb
+cd Geonumb
+chmod 777 key.sh
+chmod 777 Geonumb.sh
+cd $HOME
+echo -e "
+\e[1;30m┌═══════════════════┐
+█\e[1;32m hakkuframework... \e[1;30m█
+└═══════════════════┘\e[0m"
+pip install --upgrade pip
+pip install distutils
+pip install shutil
+pip install getopt
+git clone https://github.com/Scorpio28-Official/hakkuframework
+echo -e ""
+echo -e ""
 echo -e ""
 echo -e "
 \e[1;30m┌═══════════════════════════════════════┐
